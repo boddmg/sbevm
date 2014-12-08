7f4cffeb70d4bb0e862d466821b31442f69634a2
boddmg <boddmg@gmail.com>
2014-12-09 03:46:44 +0800

    Finish the vm demo.
    Signed-off-by: boddmg <boddmg@gmail.com>

--- experiment/sbEVM.py
+++ experiment/sbEVM.py
@@ -1,14 +1,66 @@
 stack = []
-instructionSet = ["in","out",
-                  "add","sub","mul","div",  #computing symbol
+
+class Stack():
+    def __init__(self,init = []):
+        self.data = init
+
+    def push(self,item):
+        self.append(item)
+
+    def pop(self):
+        return self.pop()
+
+# instruction structure: {"cmd":cmd,"data":data}
+INSTRUCTION_SET = ["in","out",
+                  "add","sub","mul","div","or","and","xor",  #computing symbol
                   "push","pop",
                   "jgt","jge", "jlt","jeq","jne","jmp" ]
+
 program = []
 mem = [0 for n in range(1000)]
 
-def excete():
+def read_from_mem(address):
+    return
 
+class sbVM():
+    def __init__(self,program):
+        self.stack = Stack()
+        self.program = program
+
+    def _push(self,data):
+        address = data
+        self.stack.push(mem[address])
+
+    def _pop(self,data):
+        value = self.stack.pop()
+        address = data
+        mem[address] = value
+
+    def binary_operator(self,operator):
+        value1 = self.stack.pop()
+        value2 = self.stack.pop()
+        value = operator(value1,value2)
+        self.stack.push(value)
+
+    def _add(self, data=None):
+        self.binary_operator(lambda x,y:x+y)
+
+    def _sub(self, data=None):
+        self.binary_operator(lambda x,y:x-y)
+
+    def _div(self, data=None):
+        self.binary_operator(lambda x,y:x/y)
+
+    def _mul(self, data=None):
+
+        self.binary_operator(lambda x,y:x*y)
+
+    def _or(self, data=None):
+        self.binary_operator(lambda x,y:x or y)
+
+    def _and(self, data=None):
+        self.binary_operator(lambda  x,y:x and y)
+
+def excute():
     pass
 
-
-print instructionSet
