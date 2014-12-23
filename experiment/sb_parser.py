180fedf1500adf0d394eb89ea3ffecc049454c30
boddmg <boddmg@gmail.com>
2014-12-23 08:30:34 +0800

    finish the get_first_set function.
    Signed-off-by: boddmg <boddmg@gmail.com>

--- experiment/sb_parser.py
+++ experiment/sb_parser.py
@@ -43,19 +43,33 @@ non_terminal_symbol = set([x[0] for x in derivations])
 
 
 def get_first_set(derivation):
-    first_set = {}
-    for i in non_terminal_symbol|terminal_symbol:
-        first_set[i] = {}
+    non_terminal_symbol = set([x[0] for x in derivations])
+    first_set = dict()
+    for i in terminal_symbol:
+        first_set[i] = {i}
+    for i in non_terminal_symbol:
+        first_set[i] = set()
+
     for i in derivation:
-        if i[1][0] in terminal_symbol:
-            first_set[i] = i
+        if i[1] == ['empty']:
+            first_set[i[0]].add('empty')
             continue
 
-
-
-
+    while True:
+        last_set = first_set
+        for i in derivation:
+            for j in i[1]:
+                if 'empty' not in first_set[j]:
+                    break
+                current_symbol = first_set[i[1]]
+                first_set[i[0]] = first_set[i[0]] | current_symbol.remove("empty")
+        if first_set == last_set:
+            break
     pass
 
 class Parser():
     def __init__(self):
         pass
+
+if __name__ == "__main__":
+    print get_first_set(derivations)
