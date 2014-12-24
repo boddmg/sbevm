5a851f88c0a243274bc76026edff1bd89b9cc203
boddmg <boddmg@gmail.com>
2014-12-24 11:28:41 +0800

    Finish the first set
    Signed-off-by: boddmg <boddmg@gmail.com>

--- experiment/sb_parser.py
+++ experiment/sb_parser.py
@@ -1,4 +1,5 @@
 __author__ = 'boddmg'
+from texttable import *
 
 terminal_symbol = {
     'number',
@@ -39,37 +40,56 @@ derivations = [
     ['V', ['name']],
 ]
 
-non_terminal_symbol = set([x[0] for x in derivations])
-
-
-def get_first_set(derivation):
-    non_terminal_symbol = set([x[0] for x in derivations])
-    first_set = dict()
-    for i in terminal_symbol:
-        first_set[i] = {i}
-    for i in non_terminal_symbol:
-        first_set[i] = set()
-
-    for i in derivation:
-        if i[1] == ['empty']:
-            first_set[i[0]].add('empty')
-            continue
-
-    while True:
-        last_set = first_set
-        for i in derivation:
-            for j in i[1]:
-                if 'empty' not in first_set[j]:
-                    break
-                current_symbol = first_set[i[1]]
-                first_set[i[0]] = first_set[i[0]] | current_symbol.remove("empty")
-        if first_set == last_set:
-            break
-    pass
-
 class Parser():
-    def __init__(self):
+    def __init__(self,_derivation,_terminal_symbol):
+        self.derivation = _derivation
+        self.terminal_symbol = _terminal_symbol
+        self.first_set = None
+        self.follow_set = None
+
+    def calculate_first_set(self):
+        non_terminal_symbol = set([x[0] for x in derivations])
+        first_set = dict()
+        for i in terminal_symbol:
+            first_set[i] = {i}
+        for i in non_terminal_symbol:
+            first_set[i] = set()
+
+        for i in self.derivation:
+            if i[1] == ['empty']:
+                first_set[i[0]].add('empty')
+                continue
+
+        while True:
+            last_set = str(first_set)
+            for i in self.derivation:
+                for j in i[1]:
+                    if j == 'empty':
+                        break
+                    current_symbol = first_set[j]
+                    first_set[i[0]] = first_set[i[0]] | (current_symbol-{"empty"})
+                    if 'empty' not in first_set[j]:
+                        break
+            if str(first_set) == last_set:
+                break
+        self.first_set = first_set
+        return first_set
+
+    def calculate_follow_set(self):
+
+
+
         pass
 
+
 if __name__ == "__main__":
-    print get_first_set(derivations)
+    parser = Parser(derivations,terminal_symbol)
+    first_set = parser.calculate_first_set()
+    table = Texttable()
+    table.header(['name','set'])
+    non_terminal_symbol = set([x[0] for x in derivations])
+    for i in first_set:
+        if i in non_terminal_symbol:
+            table.add_row([i,str(first_set[i])])
+    print table.draw()
+
