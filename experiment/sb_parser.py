5a152869ebfe5f502c5e4210b47e12cecd4cf78b
boddmg <boddmg@gmail.com>
2014-12-24 16:50:34 +0800

    Start to write follow_set.
    Signed-off-by: boddmg <boddmg@gmail.com>

--- experiment/sb_parser.py
+++ experiment/sb_parser.py
@@ -20,6 +20,8 @@ terminal_symbol = {
 }
 
 derivations = [
+    ['S', ['P']],
+    ['S', ['empty']],
     ['P', ['B', 'P']],
     ['P', ['empty']],
     ['B', ['int', 'E']],
@@ -41,55 +43,81 @@ derivations = [
 ]
 
 class Parser():
-    def __init__(self,_derivation,_terminal_symbol):
-        self.derivation = _derivation
-        self.terminal_symbol = _terminal_symbol
+    def __init__(self,derivation,terminal_symbol):
+        self._derivations = derivation
+        self._terminal_symbol = terminal_symbol
         self.first_set = None
         self.follow_set = None
 
+    def calculate_non_terminal_symbol(self):
+        self._non_terminal_symbol = set([x[0] for x in derivations])
+        self._non_terminal_symbol.add('S')
+        return self._non_terminal_symbol
+
     def calculate_first_set(self):
-        non_terminal_symbol = set([x[0] for x in derivations])
+        non_terminal_symbol = self.calculate_non_terminal_symbol()
         first_set = dict()
         for i in terminal_symbol:
             first_set[i] = {i}
         for i in non_terminal_symbol:
             first_set[i] = set()
 
-        for i in self.derivation:
+        for i in self._derivations:
             if i[1] == ['empty']:
                 first_set[i[0]].add('empty')
                 continue
 
         while True:
-            last_set = str(first_set)
-            for i in self.derivation:
+            last_set_hash = str(first_set)
+            for i in self._derivations:
                 for j in i[1]:
                     if j == 'empty':
                         break
                     current_symbol = first_set[j]
-                    first_set[i[0]] = first_set[i[0]] | (current_symbol-{"empty"})
+                    first_set[i[0]] |= (current_symbol-{"empty"})
                     if 'empty' not in first_set[j]:
                         break
-            if str(first_set) == last_set:
+            if str(first_set) == last_set_hash:
                 break
         self.first_set = first_set
         return first_set
 
     def calculate_follow_set(self):
+        follow_set = {'S':{'$'}}
+        non_terminal_symbol = self.calculate_non_terminal_symbol()
+        for i in non_terminal_symbol | terminal_symbol:
+            follow_set[i] = set()
 
+        derivations = self._derivations
+        while True:
+            last_follow_hash = str(follow_set)
+            for i in derivations:
+                for j in range(len(i[1])):
+                    if j < len(i[1])-1 :
+                        
+                        follow_set[i[1][j]] |= self.first_set[i[1][j+1]] - {'empty'}
+                    if j == len(i[1])-1 :
+                        follow_set[i[1][j]] |= follow_set[i[0]]
 
+            if last_follow_hash == str(follow_set):
+                break
+        self._follow_set = follow_set
+        return follow_set
 
-        pass
+def print_set_dict(set_dict):
+    table = Texttable()
+    table.header(['name', 'set'])
+    for i in set_dict:
+        table.add_row([i, str(set_dict[i])])
+    print table.draw()
 
 
 if __name__ == "__main__":
     parser = Parser(derivations,terminal_symbol)
     first_set = parser.calculate_first_set()
-    table = Texttable()
-    table.header(['name','set'])
-    non_terminal_symbol = set([x[0] for x in derivations])
-    for i in first_set:
-        if i in non_terminal_symbol:
-            table.add_row([i,str(first_set[i])])
-    print table.draw()
+    follow_set = parser.calculate_follow_set()
+    non_terminal_symbol = parser.calculate_non_terminal_symbol()
+
+    print_set_dict(dict(filter(lambda (k,v):k in non_terminal_symbol,first_set.items())))
+    print_set_dict(dict(filter(lambda (k,v):k in non_terminal_symbol,follow_set.items())))
 
